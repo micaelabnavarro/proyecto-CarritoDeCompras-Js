229aa27845286c7b7464ab596cac357752e6020e
Micaela <micaelanavarro774@gmail.com>
2023-01-22 16:48:08 -0300

    proyecto final

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const contenedorProductos = document.querySelector('#contenedor-productos');
 const pintarProductos = (data) => {
     const template = document.querySelector('#template-productos').content
     const fragment = document.createDocumentFragment()
-       //console.log(template)
-     data.forEach(producto => {
+    //console.log(template)
+    data.forEach(producto => {
         //console.log(producto)
         template.querySelector('img').setAttribute('src', producto.imgUrl)
         template.querySelector('h5').textContent = producto.titulo
@@ -28,31 +28,31 @@ const pintarProductos = (data) => {
     })
     contenedorProductos.appendChild(fragment)
 }
- let carrito = {}
- 
- const detectarBotones = (data) => {
+let carrito = {}
+
+const detectarBotones = (data) => {
     const botones = document.querySelectorAll('.card button')
     botones.forEach(btn => {
         btn.addEventListener('click', () => {
             console.log(btn.dataset.id)
             const producto = data.find(item => item.id === parseInt(btn.dataset.id))
             producto.cantidad = 1
-            if(carrito.hasOwnProperty(producto.id)) {
-              producto.cantidad = carrito[producto.id].cantidad + 1
+            if (carrito.hasOwnProperty(producto.id)) {
+                producto.cantidad = carrito[producto.id].cantidad + 1
             }
             carrito[producto.id] = { ...producto }
             pintarCarrito()
         })
     })
 
- }
- const items = document.querySelector('#items')
- const pintarCarrito = () => {
+}
+const items = document.querySelector('#items')
+const pintarCarrito = () => {
     items.innerHTML = ''
-    
+
     const template = document.querySelector('#template-carrito').content
     const fragment = document.createDocumentFragment()
-    
+
     Object.values(carrito).forEach(producto => {
         //console.log(producto)
         template.querySelector('th').textContent = producto.id
@@ -62,7 +62,7 @@ const pintarProductos = (data) => {
         //botones
         template.querySelector('.btn-info').dataset.id = producto.id
         template.querySelector('.btn-danger').dataset.id = producto.id
-   
+
 
         const clone = template.cloneNode(true)
         fragment.appendChild(clone)
@@ -70,13 +70,13 @@ const pintarProductos = (data) => {
     items.appendChild(fragment)
     pintarFooter()
     accionBotones()
- }
-  //localStorage.setItem('obejto', JSON.stringify(carrito))
-  const footer = document.querySelector('#footer-carrito')
-  const pintarFooter = () => {
+}
+//localStorage.setItem('obejto', JSON.stringify(carrito))
+const footer = document.querySelector('#footer-carrito')
+const pintarFooter = () => {
 
     footer.innerHTML = ''
-    if(Object.keys(carrito).lenght === 0) {
+    if (Object.keys(carrito).lenght === 0) {
         footer.innerHTML = `<th scope="row" colspan="5">Carrito vacio con innerHtml</th>`
         return
     }
@@ -85,39 +85,55 @@ const pintarProductos = (data) => {
     const fragment = document.createDocumentFragment()
 
     //sumar cantidad y total
-   const nCantidad = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad, 0)
-   const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio, 0)
-   //console.log(nPrecio);
+    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+    const nPrecio = Object.values(carrito).reduce((acc, { cantidad, precio }) => acc + cantidad * precio, 0)
+    //console.log(nPrecio);
 
-   template.querySelectorAll('td')[0].textContent = nCantidad
-   template.querySelector('span').textContent = nPrecio
-    
+    template.querySelectorAll('td')[0].textContent = nCantidad
+    template.querySelector('span').textContent = nPrecio
 
-   const clone = template.cloneNode(true)
-   
-   fragment.appendChild(clone)
 
-   footer.appendChild(fragment)
+    const clone = template.cloneNode(true)
 
-   const boton = document.querySelector('#vaciar-carrito')
-   boton.addEventListener('click', () => {
-    carrito = {}
-    pintarCarrito()
-   })
+    fragment.appendChild(clone)
 
-  }
+    footer.appendChild(fragment)
 
-  const accionBotones = () => {
+    const boton = document.querySelector('#vaciar-carrito')
+    boton.addEventListener('click', () => {
+        Swal.fire({
+            title: '¿Está seguro de eliminar los productos seleccionados?',
+            icon: 'warning',
+            showCancelButtom: true,
+            confirmButtomText: 'Si, estoy seguro',
+            cancelButtomText: 'No, no quiero eliminar',
+
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: 'Has vaciado el carrito',
+                    icon: 'success',
+
+                })
+            } carrito = {}
+            pintarCarrito()
+
+        })
+    })
+
+}
+
+const accionBotones = () => {
     const botonesAgregar = document.querySelectorAll('#items .btn-info')
     const botonesEliminar = document.querySelectorAll('#items .btn-danger')
 
     botonesAgregar.forEach(btn => {
         btn.addEventListener('click', () => {
             console.log(btn.dataset.id)
-           const producto = carrito[btn.dataset.id]
-           producto.cantidad ++
-           carrito[btn.dataset.id] = {...producto}
-           pintarCarrito()
+            const producto = carrito[btn.dataset.id]
+            producto.cantidad++
+            carrito[btn.dataset.id] = { ...producto }
+            pintarCarrito()
 
         })
     })
@@ -127,18 +143,20 @@ const pintarProductos = (data) => {
             //console.log('...eliminando')
             const producto = carrito[btn.dataset.id]
             producto.cantidad--
-            if(producto.cantidad === 0) {
+            if (producto.cantidad === 0) {
                 delete carrito[btn.dataset.id]
 
             } else {
-                carrito[btn.dataset.id] = {...producto}
+                carrito[btn.dataset.id] = { ...producto }
 
-            } 
+            }
             pintarCarrito()
         })
     })
-  
-   
+
+
+
+
 }
 
 
